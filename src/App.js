59761ab40d8a6bcaa8d6715be19eacc1ecc5a5c7
leeorGolan <80627632+leeorGolan@users.noreply.github.com>
2022-09-15 23:40:55 +0300

    red

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Box from "@mui/material/Box";
 import ModalOne from './components/ModalOne';
+import { Typography } from '@mui/material';
 //https://cdnb.artstation.com/p/assets/images/images/017/238/581/large/leeor-golan-arrowboy2.jpg?1555176369
 
 
@@ -9,18 +10,9 @@ function App() {
   return (
     <Box display ='flex' flexDirection='column' justifyContent='center' alignItems='center'>
       <h1>My first deppppploy</h1>
+   
       <h1>My first deppppploy</h1>
-      <h1>My first deppppploy</h1>
-      <h1>My first deppppploy</h1>
-      <h1>My first deppppploy</h1>
-      <h1>My first deppppploy</h1>
-      <h1>My first deppppploy</h1>
-      <h1>My first deppppploy</h1>
-      <h1>My first deppppploy</h1>
-<h1>My first deppppploy</h1>
-<h1>My first deppppploy</h1>
-<h1>My first deppppploy</h1>
-      <h1>My first deppppploy</h1>
+      <Typography sx={{color:'red'}}>My first deppppploy</Typography>
       <h1>My first deppppploy</h1>
       <h1>My first deppppploy</h1>
       <ModalOne/>
