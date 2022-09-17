30403a41df461439d0f1f3531d57d18393593e21
leeorGolan <80627632+leeorGolan@users.noreply.github.com>
2022-09-17 23:06:10 +0300

    modalfix

--- src/components/ModalOne.js
+++ src/components/ModalOne.js
@@ -6,16 +6,17 @@ import Modal from '@mui/material/Modal';
 
 const style = {
   position: 'absolute',
-  top: '220%',
+  
   left: '50%',
-  transform: 'translate(-50%,-50%)',
  
-  width: 1100,
-  height:'auto',
+  transform: 'translate(-50%)',
+  width: "calc(100vw - 560px)",
+  height:"auto",
   bgcolor: 'background.paper',
-  border: '2px solid #000',
+  border: '0px solid #000',
   boxShadow: 24,
   p: 4,
+  m:4
 };
 
 export default function BasicModal() {
@@ -37,7 +38,13 @@ export default function BasicModal() {
             <Box display='flex' flexDirection='column' justifyContent='center' alignItems='center'   >
           <img src='https://cdna.artstation.com/p/assets/images/images/017/001/122/large/leeor-golan-openerrigbuilder2.jpg?1554279507' style={{width:"100%",margin:30}} />
           <img src='https://cdnb.artstation.com/p/assets/images/images/018/180/723/large/leeor-golan-comp-3.jpg?1558552782' style={{width:"100%"}} />
+          <img src='https://cdnb.artstation.com/p/assets/images/images/018/180/723/large/leeor-golan-comp-3.jpg?1558552782' style={{width:"100%"}} />
           <img src='https://cdnb.artstation.com/p/assets/images/images/018/440/003/large/leeor-golan-comp2.jpg?1559381997' style={{width:"100%"}} />
+          <img src='https://cdna.artstation.com/p/assets/images/images/017/002/158/large/leeor-golan-poses-02.jpg?1554279299' style={{width:"100%"}} />
+          <img src='https://cdnb.artstation.com/p/assets/images/images/018/180/723/large/leeor-golan-comp-3.jpg?1558552782' style={{width:"100%"}} />
+          <img src='https://cdnb.artstation.com/p/assets/images/images/018/440/003/large/leeor-golan-comp2.jpg?1559381997' style={{width:"100%"}} />
+          <img src='https://cdna.artstation.com/p/assets/images/images/017/002/158/large/leeor-golan-poses-02.jpg?1554279299' style={{width:"100%"}} />
+          <img src='https://cdna.artstation.com/p/assets/images/images/017/002/158/large/leeor-golan-poses-02.jpg?1554279299' style={{width:"100%"}} />
           </Box>
           </Box>
       </Modal>
